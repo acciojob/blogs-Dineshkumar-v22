ba3b489bb5c8303b5ec760bde8233d32b30eeb64
Dinesh <dk159219@gmail.com>
2023-01-14 20:27:22 +0530

    try

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -18,19 +18,32 @@ public class BlogService {
     @Autowired
     BlogRepository blogRepository1;
 
+
     @Autowired
-    ImageService imageService1;
+    ImageService imageService;
+
 
     @Autowired
     UserRepository userRepository1;
 
     public List<Blog> showBlogs(){
         //find all blogs
+        return blogRepository1.findAll();
+
 
     }
 
+
     public void createAndReturnBlog(Integer userId, String title, String content) {
+
+        Blog blog = new Blog(title, content);
+        User user = userRepository1.findById(userId).get();
+        List<Blog> listofblogs = user.getBlogList();
+        listofblogs.add(blog);
+        user.setBlogList(listofblogs);
         //create a blog at the current time
+        blogRepository1.save(blog);
+        userRepository1.save(user);
 
         //updating the blog details
 
@@ -38,15 +51,28 @@ public class BlogService {
 
     }
 
+
     public Blog findBlogById(int blogId){
+        Blog blog=blogRepository1.findById(blogId).get();
+        return blog;
         //find a blog
     }
 
     public void addImage(Integer blogId, String description, String dimensions){
+        Image image=new Image(description,dimensions);
+        Blog blog=blogRepository1.findById(blogId).get();
+        List<Image> AllImages=blog.getImageList();
+        AllImages.add(image);
+        blog.setImageList(AllImages);
+        //imageRepository.save(image);
+        blogRepository1.save(blog);
         //add an image to the blog after creating it
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        if(blogRepository1.findById(blogId)!=null) {
+            blogRepository1.deleteById(blogId);
+        }
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,30 +1,79 @@
 package com.driver.services;
 
 import com.driver.models.*;
+import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ImageService {
     @Autowired
     ImageRepository imageRepository2;
 
+    @Autowired
+    BlogRepository blogRepository;
+
+
     public Image createAndReturn(Blog blog, String description, String dimensions){
         //create an image based on given parameters and add it to the imageList of given blog
+        Image image = new Image(description,dimensions);
+        image.setBlog(blog);
+
+        // this will handle the base condition;
+        //if your imagelist is null
+
+        List<Image> listofimages= blog.getImageList();
+        if (Objects.isNull(listofimages)) {
+            listofimages = new ArrayList<>();
+        }
+        listofimages.add(image);
+        blog.setImageList(listofimages);
+        imageRepository2.save(image);
+        blogRepository.save(blog);
+        return image;
+
     }
+
+
+
 
     public void deleteImage(Image image){
+        Blog blog = image.getBlog();
+
+        List<Image> list = blog.getImageList();
+
+        list.remove(image);
+
+        blog.setImageList(list);
+
+        imageRepository2.delete(image);
 
     }
 
+
     public Image findById(int id) {
+        Image image=imageRepository2.findById(id).get();
+        return image;
 
     }
 
     public int countImagesInScreen(Image image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
         //In case the image is null, return 0
+        if(image==null){
+            return 0;
+        }
+        int dim=Integer.parseInt(image.getDimensions());
+        int totaldim=Integer.parseInt(screenDimensions);
+        int count=totaldim/dim;
 
+
+        return count;
     }
 }
